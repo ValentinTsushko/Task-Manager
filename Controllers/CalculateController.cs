248fcb22e81c731cd079362ad497df346fb02edb
MaAndTs <tsuandmal@gmail.com>
2025-01-19 17:42:38 +0100

    Calculator Update

--- Controllers/CalculateController.cs
+++ Controllers/CalculateController.cs
@@ -7,45 +7,53 @@ namespace Task_Manager.Controllers
 {
     public class CalculateController : Controller
     {
-
         [HttpGet]
             public IActionResult Calculate()
             {
                 return View(new InputModel());
             }
 
-            [HttpPost]
-            public IActionResult Calculate(InputModel model)
+        [HttpPost]
+        [Route("Calculate/Calculate")]
+        public IActionResult Calculate(InputModel model)
+        {
+        if (!string.IsNullOrEmpty(model.UserInput))
+        {
+            try
             {
-            bool ik = !string.IsNullOrEmpty(model.UserInput);
-                if (!string.IsNullOrEmpty(model.UserInput))
-                {
-                    try
-                    {
-                        // Выполняем вычисление выражения
-                        var result = EvaluateExpression(model.UserInput);
-                        model.UserInput = result.ToString();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Если возникла ошибка, отображаем сообщение
-                        model.UserInput = $"Ошибка: {ex.Message}";
-                    }
-                }
-                else
-                {
-                    model.UserInput = "Введите выражение!";
-                }
-
-                return View(model);
+                // Выполняем вычисление выражения
+                var result = EvaluateExpression(model.UserInput);
+                model.UserInput = result.ToString();
             }
-
-            private double EvaluateExpression(string expression)
+            catch (Exception ex)
             {
-                // Используем DataTable для вычисления выражения
-                var dataTable = new DataTable();
-                var value = dataTable.Compute(expression, string.Empty);
-                return Convert.ToDouble(value);
+            // Если возникла ошибка, отображаем сообщение
+            model.UserInput = $"Ошибка: {ex.Message}";
             }
         }
+        else
+        {
+            model.UserInput = "Введите выражение!";
+        }
+
+        return View(model);
+        }
+
+
+        [HttpPost]
+        [Route("Calculate/FieldClear")]
+        public IActionResult FieldClear(InputModel model) 
+        {
+            model.UserInput = string.Empty;
+            return View("Calculate", model);
+        }
+
+        private double EvaluateExpression(string expression)
+        {
+            // Используем DataTable для вычисления выражения
+            var dataTable = new DataTable();
+            var value = dataTable.Compute(expression, string.Empty);
+            return Convert.ToDouble(value);
+        }
     }
+}
